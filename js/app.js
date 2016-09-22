7604d05797aa31fab343d34c754641c31df107a2
littlewormholes <kolicaoleole@gmail.com>
2016-09-22 09:59:55 +0200

    switch to angular

--- js/app.js
+++ js/app.js
@@ -1,3 +1,30 @@
+var app = angular.module("app",  ['ngRoute'])
+
+.config(function($routeProvider, $locationProvider) {
+    $routeProvider.when('/', {
+        templateUrl: '/pages/landing.html',
+        title: 'Welcome'
+    })
+    .when('/cv', {
+        templateUrl: '/pages/cv.html',
+        title: 'CV'
+    })
+});
+
+app.controller('MainController', ['$scope', '$location', function ($scope, $location){
+    $scope.nav = function (hash) {
+        $location.path(hash);
+    }
+}]);
+
+app.run(['$location', '$rootScope', function($location, $rootScope) {
+    $rootScope.$on('$routeChangeSuccess', function (event, current, previous) {
+        if (current.hasOwnProperty('$$route')) {
+            $rootScope.title = current.$$route.title;
+        }
+    });
+}]);
+
 function toggleSkill(targetId, element) {
     $('.tags > li > span').removeClass('active')
     $(element).addClass('active');
